a28d5d379758b11370f6874846641dcbab115e2a
cb4rros <caiobarros.eng@uel.br>
2022-07-06 23:53:34 -0300

    Att

---
+++ algoritmo_testes.py
@@ -0,0 +1,6 @@
+#Testando!
+from subprocess import run
+
+run("/vectoronboard/barometro_testes/barometro_testes")
+run("rc_calibrate_accel")
+run("/vectoronboard/barometro_testes/barometro_testes")
---
+++ main.py
@@ -0,0 +1,4 @@
+#Iniciando os trabalhos
+from subprocess import run
+
+run("rc_altitude")
